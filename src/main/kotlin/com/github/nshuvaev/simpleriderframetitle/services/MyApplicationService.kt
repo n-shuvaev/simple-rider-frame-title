e9946a0cedfb354498f5d4a4335c78fc7060a57b
GitHub Action <action@github.com>
2021-06-09 11:37:25 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/nshuvaev/simpleriderframetitle/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.nshuvaev.simpleriderframetitle.services
+
+import com.github.nshuvaev.simpleriderframetitle.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/nshuvaev/simpleriderframetitle/services/MyProjectService.kt
+++ src/main/kotlin/com/github/nshuvaev/simpleriderframetitle/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.nshuvaev.simpleriderframetitle.services
 
+import com.github.nshuvaev.simpleriderframetitle.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
