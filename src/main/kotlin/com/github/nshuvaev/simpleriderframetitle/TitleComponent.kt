cc0e1c7f465363f6436d2e63db02dac2dcd94e56
Nikolay Shuvaev <n.shuvaev@moedelo.org>
2021-06-09 14:59:20 +0300

    Update TitleComponent.kt

--- src/main/kotlin/com/github/nshuvaev/simpleriderframetitle/TitleComponent.kt
+++ src/main/kotlin/com/github/nshuvaev/simpleriderframetitle/TitleComponent.kt
@@ -5,6 +5,7 @@ import com.intellij.openapi.components.ApplicationComponent
 import org.picocontainer.MutablePicoContainer
 
 class TitleComponent : ApplicationComponent {
+    const FRAME_TITLE_BUILDER = "com.intellij.openapi.wm.impl.FrameTitleBuilder"
 
     override fun initComponent() {
         val picoContainer = ApplicationManager.getApplication().picoContainer as MutablePicoContainer
@@ -14,9 +15,5 @@ class TitleComponent : ApplicationComponent {
 
     override fun getComponentName(): String {
         return javaClass.simpleName
-    }
-
-    companion object {
-        const FRAME_TITLE_BUILDER = "com.intellij.openapi.wm.impl.FrameTitleBuilder"
     }
 }
