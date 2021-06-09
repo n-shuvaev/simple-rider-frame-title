b3eca3095b00d9ce4ee17034ef2c98fd1066459f
Nikolay Shuvaev <n.shuvaev@moedelo.org>
2021-06-09 14:47:19 +0300

    Create PathTitleBuilder.kt

---
+++ src/main/kotlin/com/github/nshuvaev/simpleriderframetitle/PathTitleBuilder.kt
@@ -0,0 +1,17 @@
+package com.nshuvaev.simpleriderframetitle
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.wm.impl.FrameTitleBuilder
+
+class PathTitleBuilder : FrameTitleBuilder() {
+
+    override fun getProjectTitle(project: Project, virtualFile: VirtualFile): String {
+        return virtualFile.getName()
+    }
+
+    override fun getFileTitle(project: Project): String {
+        return project.getBaseDir().getName()
+    }
+
+}
